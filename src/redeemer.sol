f43e596b5537a86d02b60e91e646e9d88fc69597
William Morriss <wjmelements@gmail.com>
2018-03-06 13:27:20 -0800

    more desired redeemer interface

--- src/redeemer.sol
+++ src/redeemer.sol
@@ -1,6 +1,8 @@
 pragma solidity^0.4.19;
 
-// assumption: token upgrades through redeemer, such as https://etherscan.io/address/0x642ae78fafbb8032da552d619ad43f1d81e4dd7c#code
+// assumption: token upgrades through DappHub, such as https://etherscan.io/address/0x642ae78fafbb8032da552d619ad43f1d81e4dd7c#code
 interface Redeemer {
     function redeem() external;
+    function undo() external;
+    function to() external view returns (ERC20);
 }
